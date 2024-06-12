dbbdcdcbaa1707d24cd1eeaa06f0f23a7253d36c
taki-uddin <taki-uddin@users.noreply.github.com>
2024-06-12 22:38:58 +0000

    deploy: a98971aeeab5a3210c19bf210bda2d7a5e86735f

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -19,7 +19,7 @@ const RESOURCES = {"canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "manifest.json": "a3be2f5863efd6e420f62f643bc3bc3f",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"flutter_bootstrap.js": "2aac587bfeb06990a05770c878c32493",
+"flutter_bootstrap.js": "c3aaf08ed317bf429de216e3ba1ca282",
 "version.json": "4a2d99bd8da9f5a65f9f8fe428c51e7a",
 "index.html": "5450b421dabfe6662abf30fbf56a9f99",
 "/": "5450b421dabfe6662abf30fbf56a9f99",
